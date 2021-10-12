936bc3de7a2bed85508f096f536f120ece79e62b
smhrd <smhrd@DESKTOP-HS7CS3E>
2021-10-12 16:58:09 +0900

    안녕하세요~

--- GitTest/src/com/gitTest/Test01.java
+++ GitTest/src/com/gitTest/Test01.java
@@ -6,8 +6,7 @@ public class Test01 {
 
 		System.out.println("첫 번째 테스트!");
 		System.out.println("두 번째 테스트!");
-		System.out.println("세 번째 테스트!");
-		System.out.println("왜 나는 안됨?");
+		System.out.println("안녕하모니카");
 
 
 
