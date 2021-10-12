9f8e8b967ddb0d774e2ce535ed525531b8068f95
smhrd <smhrd@DESKTOP-5RHFHQC>
2021-10-12 16:53:29 +0900

    마ㅣ지막

--- GitTest/src/com/gitTest/Test01.java
+++ GitTest/src/com/gitTest/Test01.java
@@ -4,14 +4,13 @@ public class Test01 {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
-		
-		System.out.println("세 번째 테스트!");
-		System.out.println("왜 나는 안됨?");
-=======
 		System.out.println("첫 번째 테스트!");
 		System.out.println("두 번째 테스트!");
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-KDT-Bigdata-7/Jsuj.git
+		System.out.println("세 번째 테스트!");
+		System.out.println("왜 나는 안됨?");
+
+
+
 
 
 	}
