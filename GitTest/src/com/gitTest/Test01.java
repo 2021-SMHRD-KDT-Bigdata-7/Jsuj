f2bc3d89191200c1876f8894bdcf27809fdfc9a5
smhrd <smhrd@DESKTOP-C58G7I1>
2021-10-12 17:22:33 +0900

    ghgggg

--- GitTest/src/com/gitTest/Test01.java
+++ GitTest/src/com/gitTest/Test01.java
@@ -7,12 +7,10 @@ public class Test01 {
 		System.out.println("첫 번째 테스트!");
 		System.out.println("두 번째 테스트!");
         System.out.println("안녕 하모니카");
-<<<<<<< HEAD
-        
-=======
+
         System.out.println("밤이 되었습니다. 마피아는 죽일 사람을 골라주세요.");
         System.out.println("유은씨 잘가요^^");
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-KDT-Bigdata-7/Jsuj.git
+
 	}
 
 }
